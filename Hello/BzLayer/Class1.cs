e1d9585404f422586fcadd949f3703383a40821e
Aniesh <anieshpictmun@gmail.com>
2023-10-20 11:33:32 +0000

    git commit

---
+++ Hello/BzLayer/Class1.cs
@@ -0,0 +1,10 @@
+﻿using Entity;
+using DataLayer;
+namespace BzLayer;
+public class Class1
+{
+public List<Movie> GetMovies(){
+    DataAccess dataAccess = new DataAccess();
+    return dataAccess.GetMovies();
+}
+}
