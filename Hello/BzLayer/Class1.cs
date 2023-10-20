39171b23ca3a69ef9f64fa56a4bb84e87af5bb7d
Aniesh <anieshpictmun@gmail.com>
2023-10-20 11:33:46 +0000

    git commit

--- Hello/BzLayer/Class1.cs
+++ Hello/BzLayer/Class1.cs
@@ -1,7 +1,7 @@
 ﻿using Entity;
 using DataLayer;
 namespace BzLayer;
-public class Class1
+public class MovieBz
 {
 public List<Movie> GetMovies(){
     DataAccess dataAccess = new DataAccess();
--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,4 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Entity;
-using DataLayer;
-Console.WriteLine("Hello, World!");
+using BzLayer;
+
+MovieBz bz = new MovieBz();
+List<Movie> movie = bz.GetMovies();
+if(movie!= null){
+    foreach(var m in movie){
+        Console.WriteLine($"{m.Id} {m.Name} {m.Ratings} {m.Ryear}");
+    }
+}
+else{
+    {
+        Console.WriteLine("No Movies Present!");
+    }
+}
+
